ee08d6269d4c7f31bd2dc491885a4addb71c40be
Aleksandr Tarasinsky <a.tarasinsky@gmail.com>
2016-11-20 17:49:09 +0200

    corrected rbs after Vitaly

--- discr.part1.rb
+++ discr.part1.rb
@@ -6,9 +6,9 @@ puts "Укажите коэффициент c"
 c = gets.to_f
 d = b**2 - 4 * a * c
 if d < 0
-	puts "Корней нет"
+  puts "Корней нет"
 elsif d == 0 
-	puts "D = #{d}, X1 = X2 = #{-b / (2 * a)}"
+  puts "D = #{d}, X1 = X2 = #{-b / (2 * a)}"
 else
-	puts "D = #{d}, X1 = #{(-b + Math.sqrt(d))/ (2 * a)}, X2 = #{(-b - Math.sqrt(d))/ (2 * a)}"
+  puts "D = #{d}, X1 = #{(-b + Math.sqrt(d))/ (2 * a)}, X2 = #{(-b - Math.sqrt(d))/ (2 * a)}"
 end
